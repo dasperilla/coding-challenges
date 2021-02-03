6f712873948ac96d007e4386c687e84fe2126905
Daniel Asperilla <dasperilla@gmail.com>
2021-02-03 12:22:18 +0100

    Javascript challenge completed

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -1,8 +1,42 @@
+/* eslint-disable linebreak-style */
 /**
  * Returns the adidas three stripes logo using `@` characters.
  * @param {number} width - Width of a stripe.
  * @returns {string} adidas logo.
  */
 module.exports = function(width) {
-  return `${ width }`;
+  if (width < 2) {
+    throw new TypeError('Error, minimum width is 2');
+  }
+
+  const specs = { minHeight: Math.round(Math.sqrt(width)),
+    stripesNumber: 3,
+    spacesBetweenStripes: Math.round(Math.sqrt(width)) };
+  let maxLineWidth = width * specs.stripesNumber;
+  let adidasLogoOutput = '';
+
+  Array.from({ length: specs.stripesNumber }, (_, stripeNumber) => {
+    Array.from({ length: specs.minHeight }, () => {
+      let atsPrintedInStripe = 0;
+      let columnOffset = maxLineWidth - ((stripeNumber + 1) * width) - (stripeNumber * specs.spacesBetweenStripes);
+
+      adidasLogoOutput = adidasLogoOutput.concat(Array(maxLineWidth).fill().map((__, currentColumn) => {
+        if (currentColumn === columnOffset && atsPrintedInStripe < width) {
+          atsPrintedInStripe++;
+          columnOffset++;
+
+          return '@';
+        }
+        if (atsPrintedInStripe >= width) {
+          atsPrintedInStripe = 0;
+          columnOffset = columnOffset + specs.spacesBetweenStripes;
+        }
+
+        return ' ';
+      }).toString().replace(/,/g, '').concat('\n'));
+      maxLineWidth++;
+    });
+  });
+
+  return `${ adidasLogoOutput.replace(/\n+$/, '') }`;
 };
